5f473c16be500ffba6c24f07e40245b9395a1720
Negar Baharmand <negarbaharmand@outlook.com>
2023-08-02 11:15:13 +0200

    Part 2, forEach loop

--- src/main/java/com/baharmand/NameRepository.java
+++ src/main/java/com/baharmand/NameRepository.java
@@ -3,6 +3,7 @@ package com.baharmand;
 import java.util.Arrays;
 
 public class NameRepository {
+
     private static String[] names = new String[0];
 
     public static int getSize() {
@@ -19,5 +20,24 @@ public class NameRepository {
 
     public static String[] findAll() {
         return Arrays.copyOf(names, names.length);
+    }
+
+    public static String find(final String fullName) {
+        for (String name : names) {
+            if (name.equals(fullName)) {
+                return name; //return the name if found
+            }
+        }
+        return null; //return null if the name is not found
+    }
+    public static boolean add(final String fullName) {
+        for (String name : names) {
+            if (name.equals(fullName)) {
+                return false; //return false if the array already includes the name
+            }
+        }
+        names = Arrays.copyOf(names, names.length + 1);
+        names[names.length - 1] = fullName;
+        return true; //return true as the name is added successfully
     }
 }
